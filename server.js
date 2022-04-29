45c5c1bb4fc157652bf6d195699268e424ae9634
Priscila Power <priscilapower92@gmail.com>
2022-04-29 00:24:13 -0300

    updating mongoose connection

--- server.js
+++ server.js
@@ -35,8 +35,11 @@ connectDB();
 
 if(process.env.NODE_ENV === 'production'){
     //set static folder
-    app.use(express());
+    app.use(express.static('client/build'));
 }
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 
 console.log('depois conexao');
