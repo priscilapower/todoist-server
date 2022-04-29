32223609ff2eb6478b434526c7738afb566c05f9
Priscila Power <priscilapower92@gmail.com>
2022-04-29 00:40:34 -0300

    updating mongoose connection

--- server.js
+++ server.js
@@ -6,6 +6,19 @@ const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+app.use(cors);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE_HOST, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");
+}).catch(err => {
+    console.log('Could not connect to the database. Error...', err);
+    process.exit();
+});
 
 app.get('/', (req, res) => {
     res.json({"message": "Server is running :D"});
