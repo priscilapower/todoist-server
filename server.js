f4cefadf035f576b2aab565ab6fa1efb9bfcb165
Priscila Power <priscilapower92@gmail.com>
2022-04-28 22:32:38 -0300

    updating cors use

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-app.use(cors())
+app.use(cors)
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
