1f0564a4f9b56108bb0e221124a6c2536da5a3df
Priscila Power <priscilapower92@gmail.com>
2022-04-29 00:31:18 -0300

    updating mongoose connection

--- server.js
+++ server.js
@@ -7,49 +7,29 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 app.use(cors);
-app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/main', (req, res) => {
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE_HOST, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");
+}).catch(err => {
+    console.log('Could not connect to the database. Error...', err);
+    process.exit();
+});
+
+require('./app/routes/routes.js')(app);
+
+app.route('/opa')
+    .get(function (req, res) {
+        res.json({"message": "Server is running :DDDD"});
+    });
+
+app.get('/', (req, res) => {
     res.json({"message": "Server is running :D"});
 });
-
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.DATABASE_HOST, {
-            useNewUrlParser: true
-        });
-
-        console.log('MongoDB connected!!');
-    } catch (err) {
-        console.log('Failed to connect to MongoDB', err);
-    }
-};
-
-connectDB();
-
-
-
-
-
-if(process.env.NODE_ENV === 'production'){
-    //set static folder
-    app.use(express.static('client/build'));
-}
-app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
-
-
-console.log('depois conexao');
-require('./app/routes/routes.js')(app);
-console.log('depois routes');
-
-
-console.log('depois da rota raiz');
-
-
 
 let PORT = process.env.PORT || 8088;
 
