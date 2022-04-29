5b7a074bcac6dcca3706aca1fbca76ecb85be132
Priscila Power <priscilapower92@gmail.com>
2022-04-28 22:45:07 -0300

    update config cors

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-app.use(cors)
-pp.use(express.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(cors);
+pp.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DATABASE_HOST, {
